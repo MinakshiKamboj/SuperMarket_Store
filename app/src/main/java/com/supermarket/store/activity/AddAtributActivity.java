753ad8b7cc55ab83d84c610cee261852e4c6330a
Minakshi kamboj <kamboj74096@gmail.com>
2021-11-24 16:01:26 +0530

    first

---
+++ app/src/main/java/com/supermarket/store/activity/AddAtributActivity.java
@@ -0,0 +1,17 @@
+package com.supermarket.store.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.supermarket.store.R;
+
+public class AddAtributActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edite_atribut);
+
+    }
+}
